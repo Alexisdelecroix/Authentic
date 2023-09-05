6714ac14cc505f498e68602af9a53c6143e46f9b
Delecroix <d-alexis@hotmail.fr>
2023-09-05 11:43:41 +0200

    Méthode Cookie Parser

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -91,8 +91,6 @@ const register = async (req, res) => {
     })
 }
 
-
-
 const login = async (req, res) => {
 
     const { email, password } = req.body;
@@ -144,7 +142,7 @@ const login = async (req, res) => {
 const extractBearer = authorization => {
     if (typeof authorization !== 'string') {
         return null;
-    } 
+    }
     const matches = authorization.match(/(bearer)\s+(\S+)/i);
 
     return matches ? matches[2] : null;
@@ -166,13 +164,85 @@ const dashboard = (req, res) => {
 
         console.log(decodedToken);
 
-        console.log('Accès autorisé'); 
+        console.log('Accès autorisé');
         return res.status(200).json({ message: 'Accès autorisé' });
-
-
     });
 };
 
+// const login = async (req, res) => {
+
+//     const { email, password } = req.body;
+
+
+//     if (!email || !password) {
+//         return res.status(400).json({ error: 'Email et mot de passe sont requis' });
+//     }
+
+//     // Préparation de la requête SQL pour récupérer l'utilisateur par son email
+//     const query = 'SELECT * FROM admin WHERE email = ?';
+
+//     // Exécuter la requête SQL
+//     conn.query(query, [email], (err, results) => {
+//         if (err) {
+//             return res.status(500).json({ error: err.message });
+//         }
+
+//         // Vérifier si un utilisateur correspondant à l'email
+//         if (results.length === 0) {
+//             return res.status(401).json({ error: 'Utilisateur non trouvé' });
+//         }
+
+//         // Extraire le premier utilisateur trouvé (s'il y en a plusieurs)
+//         const admin = results[0];
+
+//         // Comparer le mot de passe fourni avec le mot de passe haché de l'administrateur
+//         bcrypt.compare(password, admin.password, (bcryptErr, result) => {
+//             if (bcryptErr) {
+//                 return res.status(500).json({ error: 'Erreur de comparaison de mot de passe' });
+//             }
+
+//             if (!result) {
+//                 return res.status(401).json({ error: 'Mot de passe incorrect' });
+//             }
+
+//             // Si l'authentification réussit, générer le token JWT
+//             jwt.sign({ email: emailEnv }, KEY, { expiresIn: '1h' }, (jwtErr, token) => {
+//                 if (jwtErr) {
+//                     return res.status(500).json({ error: 'Erreur de génération du token JWT' });
+//                 }
+
+//                 // Ajouter le token JWT en tant que cookie
+//                 res.cookie('token', token, { expires: new Date(Date.now() + 3600000), httpOnly: true });
+
+//                 // Renvoyer une réponse réussie
+//                 // res.status(200).json({ message: 'Authentification réussie' });
+//                 res.status(200).json({message: 'Authentification réussie, voici votre token :', token });
+//             })
+//         })
+//     });
+// }
+
+// const dashboard = (req, res) => {
+//     // Extraire le token du cookie nommé "token"
+//     const token = req.cookies.token;
+
+//     if (!token) {
+//         return res.status(401).json({ message: 'Token introuvable' });
+//     }
+
+//     jwt.verify(token, KEY, (err, decodedToken) => {
+//         if (err) {
+//             return res.status(401).json({ message: 'Mauvais token' });
+//         }
+
+//         req.decodedToken = decodedToken;
+
+//         console.log(decodedToken);
+
+//         console.log('Accès autorisé');
+//         return res.status(200).json({ message: 'Accès autorisé' });
+//     });
+// };
 
 
 //Routes
